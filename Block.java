405eb321476cb42335153acfd3fb260057c49b38
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-03-19 14:11:48 -0700

    Add Block and Chunk classes to be populated once slides are up.

---
+++ Block.java
@@ -0,0 +1,13 @@
+/** ********************************************************************************
+ * File: Block.java
+ * Authors: Chloe Mei Stabinsky, Hunter Swanson, Brian Cho
+ * Class: CS 4450 - Computer Graphics
+ *
+ * Assignment: Final Project
+ * Date last modified: 3/19/19
+ *
+ * Purpose: Represents a single block. Holds its type, state, and location.
+ ********************************************************************************* */
+public class Block {
+    
+}
---
+++ Chunk.java
@@ -0,0 +1,13 @@
+/** ********************************************************************************
+ * File: Chunk.java
+ * Authors: Chloe Mei Stabinsky, Hunter Swanson, Brian Cho
+ * Class: CS 4450 - Computer Graphics
+ *
+ * Assignment: Final Project
+ * Date last modified: 3/19/19
+ *
+ * Purpose: Represents a chunk, which will hold a number of blocks at once.
+ ********************************************************************************* */
+public class Chunk {
+    
+}
