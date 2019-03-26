baa5fd26e7278ff5adb7f414a899523081991aba
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-03-26 10:03:19 -0700

    Remove all Polygon and Polyhedron stuff. Uneeded.

--- DisplayWindow3D.java
+++ DisplayWindow3D.java
@@ -1,18 +1,14 @@
-
 /** ********************************************************************************
  * File: DisplayWindow3D.java
  * Authors: Chloe Mei Stabinsky, Hunter Swanson, Brian Cho
  * Class: CS 4450 - Computer Graphics
  *
  * Assignment: Final Project
- * Date last modified: 3/20/19
+ * Date last modified: 3/26/19
  *
  * Purpose: To display the screen and its objects.
  ********************************************************************************* */
-import java.util.ArrayList;
-import java.util.Iterator;
 import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.util.vector.Vector3f;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
@@ -24,7 +20,6 @@ public class DisplayWindow3D {
 
     private FPCameraController camera;
     private DisplayMode displayMode;
-    private ArrayList<Polyhedron> shapes;
     private Chunk chunk;
 
     private enum Side {
@@ -37,107 +32,8 @@ public class DisplayWindow3D {
      */
     public DisplayWindow3D() {
         camera = new FPCameraController(0f, 0f, 0f);
-        shapes = new ArrayList<>();
         
-        initializeObjects();
-    }
-
-    /**
-     * Method: initializeObjects() Purpose: Create all objects here.
-     */
-    private void initializeObjects() {
-        drawCube(0, 0, 0, 0.5f);
-        drawCube(2, 1, 1, 0.5f);
-    }
-
-    /**
-     * Method: createSide(Vector3f location, Polyhedron newCube, float
-     * sideLength, float r, float b, float g, Side side) Purpose: Create a side
-     * for a polygon.
-     *
-     * @param location The location on the coordinate axis. Represents (x, y,
-     * z).
-     * @param newCube The cube that these sides are going to combine into.
-     * @param sideLength The length of the side for the cube.
-     * @param r The r value for the color.
-     * @param b The b value for the color.
-     * @param g The g value for the color.
-     * @param side The Enum side that checks if it's for the top, bottom, left,
-     * right, front, or back.
-     */
-    private void createSide(Vector3f location, Polyhedron newCube, float sideLength, float r, float b, float g, Side side) {
-        Polygon newSide = new Polygon(r, b, g);
-        float x = location.getX();
-        float y = location.getY();
-        float z = location.getZ();
-
-        if (null != side) {
-            switch (side) {
-                case TOP:
-                    newSide.addPoint(new Vector3f(x + sideLength, y + sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y + sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y + sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x + sideLength, y + sideLength, z + sideLength));
-                    newCube.addSide(newSide);
-                    break;
-                case BOTTOM:
-                    newSide.addPoint(new Vector3f(x + sideLength, y - sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y - sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y - sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x + sideLength, y - sideLength, z - sideLength));
-                    break;
-                case FRONT:
-                    newSide.addPoint(new Vector3f(x + sideLength, y + sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y + sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y - sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x + sideLength, y - sideLength, z + sideLength));
-                    break;
-                case BACK:
-                    newSide.addPoint(new Vector3f(x + sideLength, y - sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y - sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y + sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x + sideLength, y + sideLength, z - sideLength));
-                    break;
-                case LEFT:
-                    newSide.addPoint(new Vector3f(x - sideLength, y + sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y + sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y - sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x - sideLength, y - sideLength, z + sideLength));
-                    break;
-                case RIGHT:
-                    newSide.addPoint(new Vector3f(x + sideLength, y + sideLength, z - sideLength));
-                    newSide.addPoint(new Vector3f(x + sideLength, y + sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x + sideLength, y - sideLength, z + sideLength));
-                    newSide.addPoint(new Vector3f(x + sideLength, y - sideLength, z - sideLength));
-                    break;
-                default:
-                    break;
-            }
-        }
-        newCube.addSide(newSide);
-    }
-
-    /**
-     * Method: drawCube(float x, float y, float z, float sideLength) Purpose:
-     * Draws the cubes using the createSide() methods.
-     *
-     * @param x The x value of the cube.
-     * @param y The y value of the cube.
-     * @param z The z value of the cube.
-     * @param sideLength The length of the sides of the cube.
-     */
-    private void drawCube(float x, float y, float z, float sideLength) {
-        Polyhedron newCube = new Polyhedron();
-        Vector3f location = new Vector3f(x, y, z);
-
-        createSide(location, newCube, sideLength, 1f, 0f, 0f, Side.TOP);
-        createSide(location, newCube, sideLength, 0f, 1f, 0f, Side.BOTTOM);
-        createSide(location, newCube, sideLength, 1f, 0f, 1f, Side.LEFT);
-        createSide(location, newCube, sideLength, 1f, 1f, 0f, Side.RIGHT);
-        createSide(location, newCube, sideLength, 1f, 0f, 0.5f, Side.FRONT);
-        createSide(location, newCube, sideLength, 0f, 1f, 1f, Side.BACK);
-
-        shapes.add(newCube);
+//        initializeObjects();
     }
 
     /**
@@ -252,46 +148,4 @@ public class DisplayWindow3D {
         }
         Display.destroy();
     }
-
-    /**
-     * Method: render() Purpose: Renders the objects for the display.
-     */
-    private void drawPolyhedron(Polyhedron p) {
-        ArrayList<Polygon> sides = p.getSides();
-        for (Iterator<Polygon> iterator = sides.iterator(); iterator.hasNext();) {
-            drawPolygon(iterator.next());
-        }
-    }
-
-    /**
-     * Method: drawPolygon(Polygon p) Purpose: Draws the polygon.
-     *
-     * @param p The polygon to draw.
-     */
-    private void drawPolygon(Polygon p) {
-        ArrayList<Vector3f> s = p.getPoints();
-        glColor3f(p.getRed(), p.getGreen(), p.getBlue());
-        glBegin(GL_POLYGON);
-        for (Iterator<Vector3f> iterator = s.iterator(); iterator.hasNext();) { //draw each polygon, then fill it appropriately. Changes the color before drawing and doesn't change it until the next polygon
-            Vector3f f = iterator.next();
-            glVertex3f(f.getX(), f.getY(), f.getZ());
-        }
-        glEnd();
-    }
-
-    /**
-     * Method: render() Purpose: Renders all objects.
-     */
-    private void render() {
-        try {
-
-//            for (Iterator<Polyhedron> iterator = shapes.iterator(); iterator.hasNext();) {
-//                drawPolyhedron(iterator.next());
-//            }
-
-        } catch (Exception e) {
-
-        }
-    }
-
 }
